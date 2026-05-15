2f6d5658286f8b234d4826aa1436de436c4cbeeb
Dixit2307 <marudixit18@gmail.com>
2026-05-15 16:08:12 +0530

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,75 @@
+# Convert celcius to fahrenheit
+N=input("type tampracher in celcius ")
+print ("your answer is :", (int(N)*9/5)+32)
+
+# Find remainder using %
+
+x=int(input("Enter a number :"))
+y=int(input("enter second number:"))
+c=x%y
+print("your remainder is :",c)
+
+# calculater simple intrest
+
+a=float(input("Enter the Amount: "))
+b=float(input("Enter the intrest rate(%): "))
+c=float(input("Enter  time in year: "))
+d=a*b*c/100
+print("your intrest is",d)
+
+
+#Calculate area fo ractngle
+
+a=int(input("enter the lenght:"))
+b=int(input("enter width:"))
+c=a*b
+print("area of your ractangle is",c)
+
+# calculate area of ciecle
+
+r=float(input("Enter the radiase of ciecle:"))
+a=3.14*r*r
+print("this is the area of your circle",a)
+
+# check greater number betweeen two number
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number"))
+if(a>b):
+ print("largest number is A")
+else:
+ print("The largest number is B")
+
+ # check if number is equal or not
+
+a=int(input("Enter First number:"))
+b=int(input("Enter second number:"))
+if(a==b):
+ print("bothe number is equal")
+else:
+ print("bothe number is not equal")
+ 
+# Voting aligablity
+
+age=int(input("Enter your age:"))
+if(age>=18):
+    print("The candidate is eligable for voting")
+else:
+    print("the candidate is not eligable for voting")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
