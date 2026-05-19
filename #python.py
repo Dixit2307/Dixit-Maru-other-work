c1bdaa661c7a5e702ffec7cd96569b6104c5fe9c
Dixit2307 <marudixit18@gmail.com>
2026-05-19 15:32:39 +0530

    Validate input

---
+++ #python.py
@@ -0,0 +1,10 @@
+#python 
+# Validate input(age>18 AND city=="surat")
+age=int(input("Enter your age:"))
+city=input("enter your city:")
+
+if age>18 and city=="surat":
+    print("your input is validate")
+else:
+    print("your input is not validate") 
+
