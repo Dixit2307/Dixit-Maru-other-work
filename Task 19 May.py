8b53e47f03c65cc14576780cfec0c8f1456c565c
Dixit2307 <marudixit18@gmail.com>
2026-05-19 13:16:04 +0530

    Task 19May

---
+++ Task 19 May.py
@@ -0,0 +1,21 @@
+# check pass or fail based on marks
+
+num=int(input(" Enter your Marks"))
+
+if num>=33:
+ print("The student pass in Exam ")
+else:
+  print("This student is Fail in Exam")  
+
+# check if number is between 1-100
+
+num=int(input("Enter a number"))
+
+if num >=1 and num <=100:
+        print(f"{num} is between 1 and 100.")
+else:
+        print(f"{num} is not between 1 and 100.")
+
+# login system
+
+
