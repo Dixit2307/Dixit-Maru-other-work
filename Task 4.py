703c5f4e6034de3c802ab087a1ffe8a867dc20ce
Dixit2307 <marudixit18@gmail.com>
2026-05-19 13:46:18 +0530

    Login System Task

---
+++ Task 4.py
@@ -0,0 +1,14 @@
+
+# Login system (username + password)
+
+Accept_username="Dixit Maru"
+Accept_password="8717gseb"
+
+username=input("Enter account username:")
+password=input("Enter password:")
+
+if username==Accept_username and password==Accept_password:
+    print("Your Login successful! Welcome back on your Account")
+else:
+    print("Login Failed. Invalid username or password")
+
